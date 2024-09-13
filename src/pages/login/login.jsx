46a053e544a4db32df90f5a3e58eee9e8c0b55a0
purch1 <ucheprince26@gmail.com>
2024-09-13 18:41:43 +0100

    My first commit

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,95 @@
+function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-l shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login
+          <span className="text-blue-500"> ChatApp</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            {"Don't"} have a account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
+
+// STARTED TEMPLATE
+// function Login() {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//       <div className="w-full p-6 rounded-l shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+//         <h1 className="text-3xl font-semibold text-center text-gray-300">
+//           Login
+//           <span className="text-blue-500"> ChatApp</span>
+//         </h1>
+
+//         <form>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base">Username</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="Enter username"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+//           <div>
+//             <label className="label p-2">
+//               <span className="text-base">Password</span>
+//             </label>
+//             <input
+//               type="text"
+//               placeholder="Enter password"
+//               className="w-full input input-bordered h-10"
+//             />
+//           </div>
+
+//           <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+//             {"Don't"} have a account?
+//           </a>
+
+//           <div>
+//             <button className="btn btn-block btn-sm mt-2">Login</button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
